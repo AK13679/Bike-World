5e2e258f6d1c1e0a67099e6009921b4473e59529
Abhishek <akabhishek389@gmail.com>
2017-07-31 13:09:51 +0530

    changed

--- BikeWorld/Scripts/DataTables/jquery.dataTables.js
+++ BikeWorld/Scripts/DataTables/jquery.dataTables.js
@@ -5121,7 +5121,7 @@
 	
 		if ( tn ) {
 			msg += '. For more information about this error, please see '+
-			'http://datatables.net/tn/'+tn;
+			'https://datatables.net/tn/'+tn;
 		}
 	
 		if ( ! level  ) {
